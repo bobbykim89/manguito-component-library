dc40e4f1a552be27e572696c2ed37a23f63c1311
Sihun Kim <skim585@asu.edu>
2022-12-06 02:08:23 -0700

    refactor(dist/): remove dist folder

--- dist/assets/Accordion.story.db962575.js
+++
@@ -1,8 +0,0 @@
-const html = '<h1 id="accordion" tabindex="-1">Accordion <a class="header-anchor" href="#accordion" aria-hidden="true">#</a></h1>\n<h2 id="accordion-alpha" tabindex="-1">Accordion-alpha <a class="header-anchor" href="#accordion-alpha" aria-hidden="true">#</a></h2>\n<p>Accordion alpha description</p>\n<h2 id="accordion-beta" tabindex="-1">Accordion-beta <a class="header-anchor" href="#accordion-beta" aria-hidden="true">#</a></h2>\n<p>Accordion alpha description</p>\n';
-const frontmatter = {};
-const relativePath = "src/stories/components/accordion/Accordion.story.md";
-export {
-  frontmatter,
-  html,
-  relativePath
-};
