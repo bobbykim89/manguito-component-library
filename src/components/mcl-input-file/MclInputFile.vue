bfabc128bf64f5bba4135c5a40d8a1e0b0e8898b
Sihun Kim <skim585@asu.edu>
2022-12-29 16:58:23 -0700

    feat(/component/mcl-input-file): initialize mc-input-file component
    initialize mcl-input-file component which will be a mcl component handling file type

---
+++ src/components/mcl-input-file/MclInputFile.vue
@@ -0,0 +1,159 @@
+<script setup lang="ts">
+import { withDefaults, computed, ref } from 'vue'
+import type {
+  ColorPalette,
+  BodyText,
+  InputType,
+  SpacingLevel,
+} from '@bobbykim/manguito-theme/theme/theme.types'
+import generateClass from '@bobbykim/manguito-theme'
+
+const props = withDefaults(
+  defineProps<{
+    identifier: string
+    displayLabel?: boolean
+    labelText?: string
+    labelSize?: BodyText
+    labelColor?: ColorPalette
+    labelBold?: boolean
+    displayBorder?: boolean
+    borderColor?: ColorPalette
+    bgColor?: ColorPalette
+    buttonText: string
+    buttonTextColor?: ColorPalette
+    buttonColor?: ColorPalette
+    displayShadow?: boolean
+    isRequired?: boolean
+    spacing?: SpacingLevel
+    accept?: string
+    modelValue?: File
+  }>(),
+  {
+    displayLabel: true,
+    labelSize: 'md',
+    labelColor: 'dark-3',
+    labelBold: true,
+    displayBorder: false,
+    borderColor: 'light-4',
+    bgColor: 'light-1',
+    buttonTextColor: 'dark-3',
+    buttonColor: 'light-4',
+    displayShadow: true,
+    isRequired: false,
+    spacing: 'md',
+    accept: 'image/jpg,image/jpeg,image/png',
+  }
+)
+
+const emit = defineEmits(['update:modelValue'])
+
+const inputFileName = ref('')
+const inputRef = ref()
+const onLabelClick = (): void => {
+  inputRef.value.click()
+}
+const onChangeFile = (e: Event) => {
+  const file = (e.target as HTMLInputElement).files[0]
+  emit('update:modelValue', file)
+  inputFileName.value = file.name
+}
+
+const getLabelClass = (
+  size: BodyText,
+  color: ColorPalette,
+  bold: boolean
+): string => {
+  /**
+   * @size - labelSize
+   * @color - labelColor
+   * @bold - labelBold
+   */
+
+  const classArray: string[] = [
+    generateClass('BODYTEXT', size),
+    generateClass('TEXTCOLOR', color),
+  ]
+
+  if (bold) {
+    classArray.push('font-bold')
+  }
+
+  return classArray.join(' ')
+}
+
+const getInputClass = (
+  bgColor: ColorPalette,
+  dBorder: boolean,
+  bColor: ColorPalette,
+  dShadow: boolean
+): string => {
+  /**
+   * @bgColor - bgColor
+   * @dBorder - displayBorder
+   * @bColor - borderColor
+   * @dShadow - displayShadow
+   */
+
+  const classArray: string[] = [
+    generateClass('BGCOLOR', bgColor),
+    generateClass('FOCUSRING', bColor),
+    'focus:ring-4 ring-offset-2 transition-all duration-300 ease-linear',
+  ]
+  if (dBorder) {
+    classArray.push('border-2')
+    classArray.push(generateClass('BORDER', bColor))
+  }
+
+  if (dShadow) {
+    classArray.push('shadow-md')
+  }
+  return classArray.join(' ')
+}
+const getButtonClass = (bColor: ColorPalette, tColor: ColorPalette): string => {
+  /**
+   * @bColor - buttonColor
+   * @tColor - textColor
+   */
+  const classArray: string[] = [
+    generateClass('BGCOLOR', bColor),
+    generateClass('TEXTCOLOR', tColor),
+  ]
+
+  return classArray.join(' ')
+}
+</script>
+
+<template>
+  <div :class="generateClass('MARGINB', spacing)">
+    <p
+      v-if="displayLabel"
+      class="inline-block mb-2xs cursor-default"
+      :class="getLabelClass(labelSize, labelColor, labelBold)"
+      @click="onLabelClick"
+      v-html="labelText"
+    ></p>
+    <label
+      :for="identifier"
+      class="w-full p-3xs outline-none flex justify-between"
+      :class="getInputClass(bgColor, displayBorder, borderColor, displayShadow)"
+    >
+      <span class="py-3xs pl-3xs text-sm">{{ inputFileName }}</span>
+      <div
+        class="px-xs py-3xs h-full hover:bg-opacity-70 transition-all duration-200 ease-linear cursor-pointer"
+        :class="getButtonClass(buttonColor, buttonTextColor)"
+        v-html="buttonText"
+      ></div>
+      <input
+        type="file"
+        :id="identifier"
+        ref="inputRef"
+        class="hidden"
+        :required="isRequired"
+        :accept="accept"
+        @change="onChangeFile"
+      />
+    </label>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/components/mcl-input/MclInput.vue
+++ src/components/mcl-input/MclInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, withDefaults } from 'vue'
+import { computed, withDefaults } from 'vue'
 import type {
   ColorPalette,
   BodyText,
