765aec738227b68b8483f8b3e02d1bd191b90f20
Sihun Kim <skim585@asu.edu>
2024-04-15 01:13:48 -0700

    feat(components/*): add vite-plugin-dts
    add vite-plugin-dts plugin and update vite.config

--- src/components/mcl-dropdown/vite.config.ts
+++ src/components/mcl-dropdown/vite.config.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
         return outputChunk.fileName.includes('mcl-dropdown')
       },
     }),
+    dts({ rollupTypes: true }),
   ],
   build: {
     lib: {
