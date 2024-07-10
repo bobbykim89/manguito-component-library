132dee276932c01930f48556ac6df2daf431f8da
Sihun Kim <skim585@asu.edu>
2024-07-10 01:52:18 -0700

    feat(*,-mcl-forms): dependency version bump, mcl-checkbox component (wip)
    bump version of vue and vite to latest version. wip) mcl-checkbox component, with option of updating string
    number using @checkbox-click emit, returning boolean value using v-model, add story page (wip)
    
    mcl-37

--- src/components/mcl-forms/lib/mcl-checkbox/MclCheckbox.vue
+++ src/components/mcl-forms/lib/mcl-checkbox/MclCheckbox.vue
@@ -1,34 +1,7 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, defineModel } from 'vue'
 import generateClass, { ColorPalette } from '@bobbykim/manguito-theme'
 import type { InputSizeType, ColorMap } from '../common/index.types'
-
-const props = withDefaults(
-  defineProps<{
-    id: string
-    inputSize?: InputSizeType
-    bgColor?: ColorPalette
-    checkedBgColor?: ColorPalette
-    checkColor?: ColorPalette
-    borderColor?: ColorPalette
-    displayShadow?: boolean
-    rounded?: boolean
-    value?: string | number
-    checked?: boolean
-    triState?: boolean
-  }>(),
-  {
-    inputSize: 'md',
-    bgColor: 'light-1',
-    checkedBgColor: 'warning',
-    checkColor: 'dark-3',
-    borderColor: 'dark-1',
-    displayShadow: false,
-    rounded: false,
-    checked: false,
-    triState: false,
-  }
-)
 
 const peerBgColor: ColorMap = {
   primary: 'peer-checked/input:bg-primary',
@@ -49,10 +22,56 @@ const peerBgColor: ColorMap = {
   black: 'peer-checked/input:bg-black',
   white: 'peer-checked/input:bg-white',
 }
+const beforeColor: ColorMap = {
+  primary: 'before:text-primary',
+  secondary: 'before:text-secondary',
+  success: 'before:text-success',
+  info: 'before:text-info',
+  warning: 'before:text-warning',
+  danger: 'before:text-danger',
+  'light-1': 'before:text-light-1',
+  'light-2': 'before:text-light-2',
+  'light-3': 'before:text-light-3',
+  'light-4': 'before:text-light-4',
+  'dark-1': 'before:text-dark-1',
+  'dark-2': 'before:text-dark-2',
+  'dark-3': 'before:text-dark-3',
+  'dark-4': 'before:text-dark-1',
+  transparent: 'before:text-transparent',
+  black: 'before:text-black',
+  white: 'before:text-white',
+}
+
+const props = withDefaults(
+  defineProps<{
+    id: string
+    inputSize?: InputSizeType
+    bgColor?: ColorPalette
+    checkedBgColor?: ColorPalette
+    checkColor?: ColorPalette
+    borderColor?: ColorPalette
+    displayShadow?: boolean
+    rounded?: boolean
+    value?: string | number
+    checked?: boolean
+  }>(),
+  {
+    inputSize: 'md',
+    bgColor: 'light-1',
+    checkedBgColor: 'warning',
+    checkColor: 'dark-3',
+    borderColor: 'dark-1',
+    displayShadow: false,
+    rounded: false,
+    checked: false,
+  }
+)
+
+const model = defineModel<boolean>()
 
 const checkboxRef = ref<HTMLInputElement>()
 const emit = defineEmits<{
-  (e: 'change', event: Event, value: string | number): void
+  (e: 'checkbox-click', event: Event, value: string | number): void
 }>()
 
 const handleCheckboxClick = () => {
@@ -61,7 +80,7 @@ const handleCheckboxClick = () => {
 
 const handleChange = (e: Event) => {
   const value = (e.target as HTMLInputElement).value
-  emit('change', e, value)
+  emit('checkbox-click', e, checkboxRef.value?.checked ? value : '')
 }
 
 const handleInputSize = computed<string>(() => {
@@ -86,7 +105,7 @@ const handleCheckboxLayout = computed<string>(() => {
   const classArray: string[] = [
     generateClass('BGCOLOR', bgColor),
     generateClass('BORDER', borderColor),
-    generateClass('TEXTCOLOR', checkColor),
+    beforeColor[checkColor],
     peerBgColor[checkedBgColor],
   ]
   if (displayShadow) {
@@ -105,27 +124,17 @@ const handleCheckboxLayout = computed<string>(() => {
       :id="id"
       ref="checkboxRef"
       type="checkbox"
-      name=""
+      :value="value"
       :checked="checked"
+      v-model="model"
       class="appearance-none peer/input"
       @change="handleChange"
     />
     <span
-      class="relative border inline-block p-3xs hover:bg-opacity-60"
+      class="relative border peer-checked/input:border-2 inline-block p-3xs hover:bg-opacity-60 peer-checked/input:hover:bg-opacity-60 peer-checked/input:before:content-['\2713'] peer-checked/input:before:inline-block peer-checked/input:before:absolute before:top-1/2 before:-translate-y-1/2 before:left-1/2 before:-translate-x-1/2 transition-colors duration-200 ease-linear"
       :class="[handleCheckboxLayout, handleInputSize]"
       @click="handleCheckboxClick"
     >
-      <svg
-        v-if="checked"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        fill="currentColor"
-      >
-        <!-- !Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
-        <path
-          d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
-        />
-      </svg>
     </span>
   </div>
 </template>
--- src/stories/components/mcl-forms/mcl-checkbox/checkbox.stories.ts
+++ src/stories/components/mcl-forms/mcl-checkbox/checkbox.stories.ts
@@ -2,24 +2,116 @@ import {
   bodyTextControllers,
   booleanControllers,
   colorControllers,
+  switchSizeControllers,
   textControllers,
 } from '@/assets/composables'
 import { MclCheckbox } from '@/components/mcl-forms/lib'
 import type { Meta, StoryObj } from '@storybook/vue3'
+import { ref } from 'vue'
 
 const meta: Meta<typeof MclCheckbox> = {
   title: 'Components/Form/MclCheckbox',
+  component: MclCheckbox,
+  argTypes: {
+    id: textControllers({
+      name: 'id',
+      required: true,
+      description: 'assigns id of input component',
+      category: 'Input Block',
+    }),
+    value: textControllers({
+      name: 'value',
+      required: false,
+      description: 'value of checkbox component',
+      category: 'Input Block',
+    }),
+    checked: booleanControllers({
+      name: 'checked',
+      required: false,
+      description: 'whether or not to have checkbox checked in default',
+      defaultValue: false,
+      category: 'Input Block',
+    }),
+    inputSize: switchSizeControllers({
+      name: 'input-size',
+      required: false,
+      defaultValue: 'md',
+      description: 'size of checkbox',
+      category: 'Component Block',
+    }),
+    bgColor: colorControllers({
+      name: 'bg-color',
+      required: false,
+      defaultValue: 'light-1',
+      description: 'background color of unchecked checkbox',
+      category: 'Component Block',
+    }),
+    checkedBgColor: colorControllers({
+      name: 'checked-bg-color',
+      required: false,
+      defaultValue: 'warning',
+      description: 'background color of checked checkbox',
+      category: 'Component Block',
+    }),
+    checkColor: colorControllers({
+      name: 'check-color',
+      required: false,
+      defaultValue: 'dark-3',
+      description: 'color of check icon',
+      category: 'Component Block',
+    }),
+    borderColor: colorControllers({
+      name: 'border-color',
+      required: false,
+      defaultValue: 'dark-1',
+      description: 'border color of checkbox',
+      category: 'Component Block',
+    }),
+    displayShadow: booleanControllers({
+      name: 'display-shadow',
+      required: false,
+      defaultValue: false,
+      description: 'whether or not to add shadow on the checkbox',
+      category: 'Component Block',
+    }),
+    rounded: booleanControllers({
+      name: 'rounded',
+      required: false,
+      defaultValue: false,
+      description: 'whether or not to have rounded corner',
+      category: 'Component Block',
+    }),
+  },
+  args: {
+    id: 'my-checkbox',
+    inputSize: 'md',
+    bgColor: 'light-1',
+    checkedBgColor: 'warning',
+    checkColor: 'dark-3',
+    borderColor: 'dark-1',
+    displayShadow: false,
+    rounded: false,
+    value: 'checkbox-value',
+    checked: false,
+  },
 }
 
 export default meta
 
 type Story = StoryObj<typeof MclCheckbox>
 export const MclCheckboxExample: Story = {
-  render: (args) => ({
+  render: (args: Meta<typeof MclCheckbox>['args']) => ({
     components: { 'mcl-checkbox': MclCheckbox },
     setup() {
-      return { args }
+      const checkboxVal = ref<string | number>('')
+      const handleCheckboxClick = (e: Event, value: string | number) => {
+        e.preventDefault()
+        checkboxVal.value = value
+      }
+      const modelVal = ref<boolean>()
+      return { args, checkboxVal, handleCheckboxClick, modelVal }
     },
-    template: '<mcl-checkbox v-bind="args"></mcl-checkbox>',
+    template:
+      '<section><div class="flex gap-2"><mcl-checkbox v-bind="args" v-model.getValue="modelVal" @checkbox-click="handleCheckboxClick"></mcl-checkbox><label :for="args.id">{{ args.value }}</label></div><div>current value is: {{ checkboxVal }}</div><div>v-model checked: {{ modelVal }}</div><div>prop checked: {{ args.checked }}</div></section>',
   }),
 }
