72973f44523834ecfac0709a9c55155a939333c6
Sihun Kim <skim585@asu.edu>
2024-12-30 17:46:15 -0700

    feat(tailwind.config): update tailwind config content
    update tailwind config to have contents as items inside components folder and stories folder inside src.
    
    mcl-55

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from 'tailwindcss'
 import { mclTheme } from './src/components/manguito-theme/lib/mclTheme'
 
 export default {
-  content: ['./src/**/*.{vue,ts,js,cjs}'],
+  content: [
+    './src/components/**/*.{vue,ts,js,cjs}',
+    './src/stories/**/*.{vue,ts}',
+  ],
   plugins: [mclTheme({})],
 } satisfies Config
