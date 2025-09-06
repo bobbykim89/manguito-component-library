af204f355ab4a092ac4ac645602fdd691166412b
Sihun Kim <skim585@asu.edu>
2025-09-05 23:44:05 -0700

    feat(stories/base/base-component/): add controller and mdx for card
    add controller for card component and setup mdx file.
    
    mcl-76

--- src/stories/Base/base-component/card/card.stories.ts
+++ src/stories/Base/base-component/card/card.stories.ts
@@ -1,3 +1,11 @@
+import {
+  bodyTextControllers,
+  booleanControllers,
+  colorControllers,
+  headingTextControllers,
+  rangeControllers,
+  textControllers,
+} from '@/assets/composables'
 import GrandCanyonImage from '@/assets/images/grandcanyon-image.jpg'
 import { Card } from '@/components/manguito-theme/lib'
 import type { Meta, StoryObj } from '@storybook/vue3'
@@ -6,6 +14,136 @@ import { computed } from 'vue'
 const meta: Meta<typeof Card> = {
   title: 'Base/Base Component/Card Component',
   component: Card,
+  argTypes: {
+    variant: colorControllers({
+      name: 'variant',
+      required: false,
+      defaultValue: 'light-1',
+      description: 'assigns background color of the card component.',
+      category: 'Card Body',
+    }),
+    border: booleanControllers({
+      name: 'border',
+      required: false,
+      defaultValue: false,
+      description: 'whether or not to add border to the card component.',
+      category: 'Card Body',
+    }),
+    borderVariant: colorControllers({
+      name: 'border-variant',
+      required: false,
+      defaultValue: 'light-4',
+      description: 'assigns borde color of the card component',
+      category: 'Card Body',
+    }),
+    rounded: booleanControllers({
+      name: 'rounded',
+      required: false,
+      defaultValue: false,
+      description:
+        'whether or not to have rounded corners of the card component',
+      category: 'Card Body',
+    }),
+    shadow: booleanControllers({
+      name: 'shadow',
+      required: false,
+      defaultValue: false,
+      description:
+        'whether or not to have drop shadow around the card component',
+      category: 'Card Body',
+    }),
+    glass: booleanControllers({
+      name: 'glass',
+      required: false,
+      defaultValue: false,
+      description:
+        'whether or not to add glassmorphism effect on the background of the card component',
+      category: 'Card Body',
+    }),
+    glassBlur: bodyTextControllers({
+      name: 'glass-blur',
+      required: false,
+      defaultValue: 'sm',
+      description: 'assigns blur filter size of the glassmorphism effect',
+      category: 'Card Body',
+    }),
+    glassOpacity: rangeControllers({
+      name: 'glass-opacity',
+      required: false,
+      defaultValue: 10,
+      description: 'assigns filter opacity of the glassmorphism effect.',
+      category: 'Card Body',
+      controlOption: {
+        min: 10,
+        max: 100,
+        step: 10,
+      },
+    }),
+    cardClass: textControllers({
+      name: 'card-class',
+      required: false,
+      description: 'assigns custom style class for card body',
+      category: 'Card Body',
+    }),
+    imageSrc: textControllers({
+      name: 'image-src',
+      required: false,
+      description: 'assigns image url of the card',
+      category: 'Image Block',
+    }),
+    imageAlt: textControllers({
+      name: 'image-alt',
+      required: false,
+      description: 'assigns image alt of the card',
+      category: 'Image Block',
+    }),
+    noImage: booleanControllers({
+      name: 'no-image',
+      required: false,
+      defaultValue: false,
+      description: 'whether or not to display image block.',
+      category: 'Image Block',
+    }),
+    title: textControllers({
+      name: 'title',
+      required: false,
+      description: 'assigns the title text of the card component',
+      category: 'Body Block',
+    }),
+    titleSize: headingTextControllers({
+      name: 'title-size',
+      required: false,
+      defaultValue: 'md',
+      description: 'assigns the size of title text',
+      category: 'Body Block',
+    }),
+    titleVariant: colorControllers({
+      name: 'title-variant',
+      required: false,
+      defaultValue: 'dark-4',
+      description: 'assigns the color of title text',
+      category: 'Body Block',
+    }),
+    noBody: booleanControllers({
+      name: 'no-body',
+      required: false,
+      defaultValue: false,
+      description: 'whether or not to have body block',
+      category: 'Body Block',
+    }),
+    header: textControllers({
+      name: 'header',
+      required: false,
+      description: 'assigns text for header block',
+      category: 'Header Block',
+    }),
+    footer: textControllers({
+      name: 'footer',
+      required: false,
+      description: 'assigns text for footer block',
+      category: 'Footer Block',
+    }),
+  },
   args: {
     variant: 'light-1',
     border: false,
