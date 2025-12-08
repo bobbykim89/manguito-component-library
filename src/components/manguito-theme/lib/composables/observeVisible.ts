e3d762bc5bafe8d415f32615a82b12a2b11e09a7
Sihun Kim <skim585@asu.edu>
2025-12-07 17:20:42 -0700

    fix(manguito-theme): clean up commented out lines
    clean up lines commented out.
    
    mcl-64

--- src/components/manguito-theme/lib/composables/observeVisible.ts
+++ src/components/manguito-theme/lib/composables/observeVisible.ts
@@ -73,31 +73,3 @@ export const observeVisibleAttr = (
     },
   )
 }
-
-// export const observeVisibleAttr = (el: Ref, fn: (...args: any[]) => void) => {
-//   return useMutationObserver(
-//     el,
-//     (mutations) => {
-//       let changed: boolean = false
-//       for (let i = 0; i < mutations.length && !changed; i++) {
-//         const mutation = mutations[i]
-//         const mutationType = mutation.type
-//         const mutationAttr = mutation.attributeName
-//         const mutationTarget = mutation.target
-//         if (mutationType === 'attributes') {
-//           changed = true
-//         } else if (mutationAttr === 'visible') {
-//           changed = true
-//         }
-//         if (changed) {
-//           const visibleAttr =
-//             (mutationTarget as HTMLElement).getAttribute('visible') === 'true'
-//               ? true
-//               : false
-//           fn(visibleAttr)
-//         }
-//       }
-//     },
-//     { attributes: true },
-//   )
-// }
